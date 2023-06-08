81be6a94bb4738b6fadef13e30d7f825d65c2980
Kd125 <kashdangar1250@gmail.com>
2023-06-07 21:52:41 -0500

    final commit

--- src/Router.js
+++ src/Router.js
@@ -1,27 +1,37 @@
-import React from 'react'
-import { Routes, Route, Navigate } from 'react-router'
-import cookie from 'cookie'
-import Home from './components/Home'
-import About from './components/About'
-import Car from './components/Car'
-import Login from './components/Login'
+import React from "react";
+import { Routes, Route, Navigate } from "react-router";
+import cookie from "cookie";
+import Home from "./components/Home";
+import About from "./components/About";
+import Car from "./components/Car";
+import Login from "./components/Login";
 
 // Write checkAuth function here
+const checkAuth = () => {
+  const cookies = cookie.parse(document.cookie);
+  return cookies["loggedIn"] ? true : false;
+};
 // Check the cookies for a cookie called "loggedIn"
 
-
 // Write ProtectedRoute function here
+const ProtectedRoute = (props) => {
+  const { component: Component, ...rest } = props;
 
-
-const Router = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/login" element={<Login/>} />
-            <Route path="/about" element={<About/>} />
-            <Route path="/car/:id" element={<Car/>} />
-        </Routes>
-    );
+  return checkAuth() === true ? (
+    <Component {...rest} />
+  ) : (
+    <Navigate to="/login" replace={true} />
+  );
 };
 
+const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<ProtectedRoute component={Home} />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/about" element={<ProtectedRoute component={About} />} />
+      <Route path="/car/:id" element={<ProtectedRoute component={Car} />} />
+    </Routes>
+  );
+};
 export default Router;
